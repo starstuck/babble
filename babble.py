424eb5a2b70bdadd09e8fbfe31d50130e2551e17
Tomasz Szarstuk <szarsti@gmail.com>
2017-10-09 19:00:32 +0100

    Add pubsub events support

--- babble.py
+++ babble.py
@@ -2,8 +2,10 @@ import logging
 import xml.etree.ElementTree as ET
 
 import sleekxmpp
+from sleekxmpp.xmlstream.handler import Callback
+from sleekxmpp.xmlstream.matcher import StanzaPath
 from sleekxmpp.componentxmpp import ComponentXMPP
-from sleekxmpp.plugins.xep_0009.binding import py2xml
+from sleekxmpp.plugins.xep_0009.binding import py2xml, _xml2py
 
 COMPONENT="metrics.localhost"
 SECRET="secret"
@@ -15,9 +17,9 @@ class Service(object):
 
     def __init__(self, xmpp):
         self.xmpp = xmpp
-        xmpp.add_event_handler("message", self._handle_message)
-        xmpp.add_event_handler("session_bind", self._handle_session_bind)
-        xmpp.add_event_handler("presence", self._handle_presence)
+        xmpp.add_event_handler('message', self._handle_message)
+        xmpp.add_event_handler('session_bind', self._handle_session_bind)
+        xmpp.add_event_handler('presence', self._handle_presence)
         xmpp.add_event_handler('session_start', self._handle_session_start)
         xmpp.add_event_handler('jabber_rpc_method_call', self._handle_jabber_rpc_method_call)
 
@@ -26,12 +28,18 @@ class Service(object):
         xmpp.register_plugin('xep_0060') # PubSub
         xmpp.register_plugin('xep_0199') # XMPP Ping
 
-        # TODO: the service should manage its roster
+        # Remove default pub/sub emssage handler, because I don't want to deal
+        # with each of them item individually. I'd rather process them in batches
+        xmpp.remove_handler('Pubsub Event: Items')
+        xmpp.register_handler(
+            Callback('Babble Pubsub Event: Items',
+                    StanzaPath('message/pubsub_event/items'),
+                    self._handle_pubsub_items))
+        
+        # TODO: check if server is keeping track of subscriptions sufficiently
 
     def _handle_message(self, msg):
-        # outgoing reply's 'from' JID.
-        logging.info("Recived message: %s", msg)
-        msg.reply("Thanks for sending\n%(body)s" % msg).send()
+        logging.info("Recived plain message from %s: %s", msg['from'])
 
     def _handle_session_bind(self, jid):
         logging.info("New session started by: %s", jid)
@@ -41,9 +49,7 @@ class Service(object):
         logging.info("Known rosters: %s", self.xmpp.roster._rosters)
 
     def _handle_session_start(self, event):
-        logging.info("Session started: %s", event)
-        #self.send_presence()
-        #self.get_roster()
+        logging.info("Session started")
 
     def _handle_jabber_rpc_method_call(self, iq):
         pid = iq['id']
@@ -62,6 +68,14 @@ class Service(object):
         # Populate the presence reply with the agent's current status.
         self.xmpp.send_presence(pto=sender, pstatus="Ready for data", pshow="chat")
 
+    def _handle_pubsub_items(self, msg):
+        mfrom = msg['from']
+        node = msg['pubsub_event']['items']['node']
+        count = len(msg['pubsub_event']['items'])
+        items = [_xml2py(x.find('{jabber:iq:rpc}value')) for x in msg['pubsub_event']['items']]
+        logging.info('Received %s event from %s (%d items, %s bytes): [%s, ...]',
+                      node, mfrom, count, len(str(msg)), str(items[0]))
+        
     def _subscribe(self, jid):
         xmpp = self.xmpp
         if jid.bare in xmpp.roster[xmpp.boundjid.bare]:
@@ -72,7 +86,7 @@ class Service(object):
                                                ptype='subscribe')
 
     def _get_config(self):
-        return {'account': 'nice-account'}
+        return {'account': {'id': 'test-account'}}
 
 
 def main ():
